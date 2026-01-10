34461bd7dc645bad8e0b65e0f5a4d991a7566b14
Daniil <8577311+shirgazee@users.noreply.github.com>
2026-01-10 23:38:56 +0200

    Создать UserController и метод Register

---
+++ GoldenCrown/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using GoldenCrown.Dtos;
+using GoldenCrown.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldenCrown.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+           var result = await _userService.RegisterAsync(request.Login, request.Name, request.Password);
+           if (result)
+           {
+               return Ok();
+           }
+           return BadRequest(new { Message = "User registration failed" });
+        }
+    }
+}
